8bd19f192fa5370bc9119be9026d2f9f1cfa0c0e
yachen <io260188@gmail.com>
2020-09-07 03:49:47 +0800

    feat: header

---
+++ src/components/common/LoginHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div></div>
+    <h1>{{ pageName }}</h1>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageName: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px 0;
+  h1 {
+    font-size: 20px;
+    font-weight: 400;
+  }
+}
+</style>
