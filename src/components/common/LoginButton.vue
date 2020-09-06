3e8544b742ab2a283d159c99086d722e3b106008
yachen <io260188@gmail.com>
2020-09-07 03:44:43 +0800

    feat: LoginButton

---
+++ src/components/common/LoginButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <div class="login-button">{{ buttonText }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  padding: 10px;
+}
+.login-button {
+  height: 45px;
+  line-height: 45px;
+  background-color: #ff9db5;
+  color: #fff;
+  font-weight: 500;
+  text-align: center;
+  border-radius: 20px;
+  cursor: pointer;
+}
+</style>
