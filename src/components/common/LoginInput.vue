e73f84a8d158fd42fdeb1616892c8c6621af2efb
yachen <io260188@gmail.com>
2020-09-07 03:50:02 +0800

    feat: input

---
+++ src/components/common/LoginInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <van-field
+      :label="label"
+      :type="type"
+      :placeholder="placeholder"
+      :rule="rule"
+      v-model="content"
+      @input="checkRules"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String
+    },
+    placeholder: {
+      type: String,
+      required: true
+    },
+    rule: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      content: ""
+    };
+  },
+  methods: {
+    checkRules(){
+      const rule = new RegExp(this.rule);
+      if (rule.test(this.content)){
+        this.$emit('inputChange', this.content);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
