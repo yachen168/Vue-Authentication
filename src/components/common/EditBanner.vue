bfc1670e80612e3d9a6ceb6d3aec4df95fdf1900
yachen <io260188@gmail.com>
2020-09-11 23:51:12 +0800

    feat: EditBanner

---
+++ src/components/common/EditBanner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="edit-banner">
+    <span>{{ title }}</span>
+    <span><slot></slot></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.edit-banner {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #fff;
+}
+</style>
