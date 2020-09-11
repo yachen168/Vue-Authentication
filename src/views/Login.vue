af0addc3254e8a29f2e9b7785065da9aef1b4484
yachen <io260188@gmail.com>
2020-09-11 23:54:33 +0800

    feat: login

---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form class="register">
+      <LoginHeader pageName="登入">
+        <div slot="right" @click="$router.push('/')">立即註冊</div>
+      </LoginHeader>
+      <LoginInput
+        label="帳號"
+        placeholder="請輸入帳號"
+        rule="^[A-z\d]{6,16}$"
+        @inputChange="loginInfo.username = $event"
+      ></LoginInput>
+      <LoginInput
+        label="密碼"
+        placeholder="請輸入密碼"
+        type="password"
+        rule="^[A-z\d]{6,16}$"
+        @inputChange="loginInfo.password = $event"
+      ></LoginInput>
+      <LoginButton
+        buttonText="登入"
+        :isDisabled="!(loginInfo.username && loginInfo.password)"
+        @register="$store.dispatch('postLogin', loginInfo)"
+      ></LoginButton>
+    </form>
+  </div>
+</template>
+
+<script>
+import LoginHeader from "@/components/common/LoginHeader";
+import LoginInput from "@/components/common/LoginInput";
+import LoginButton from "@/components/common/LoginButton";
+
+export default {
+  components: {
+    LoginHeader,
+    LoginInput,
+    LoginButton
+  },
+  data() {
+    return {
+      loginInfo: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
