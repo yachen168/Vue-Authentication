f71919a3e7749bb2eb405b5b58fee538d260552e
yachen <io260188@gmail.com>
2020-09-12 20:46:05 +0800

    refactor: webpackChunkName

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,36 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [{
-    path: "/register",
-    name: "Register",
-    component: () =>
-        import ("@/views/Register.vue")
-}];
+        path: "/",
+        name: "Register",
+        component: () =>
+            import ( /* webpackChunkName: "register" */ "@/views/Register.vue")
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () =>
+            import ( /* webpackChunkName: "login" */ "@/views/Login.vue")
+    },
+    {
+        path: "/userinfo",
+        name: "UserInfo",
+        component: () =>
+            import ( /* webpackChunkName: "user-info" */ "@/views/UserInfo.vue"),
+        meta: {
+            isToken: true
+        }
+    },
+    {
+        path: "/edit",
+        name: "Edit",
+        component: () =>
+            import ( /* webpackChunkName: "edit" */ "@/views/Edit.vue"),
+        meta: {
+            isToken: true // 在需要權限的路由裡個別設定 meta
+        }
+    }
+];
 
 const router = new VueRouter({
     mode: "history",
