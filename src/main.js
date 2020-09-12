93af1564524b471d85b92d78b9db91f1d474650b
yachen <io260188@gmail.com>
2020-09-12 17:00:06 +0800

    docs: plugins folder

--- src/main.js
+++ src/main.js
@@ -2,15 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Vant from 'vant';
-import 'vant/lib/index.css';
 
-Vue.use(Vant);
+import "./plugins/vant";
 
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app");
