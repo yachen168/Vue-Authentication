5d586d272f424d49c77ca75282408eadd6740b11
yachen <io260188@gmail.com>
2020-09-12 13:40:10 +0800

    feat: userInfo

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <UserDetail :userInfo="$store.getters.userInfo"></UserDetail>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/common/Navbar";
+import UserDetail from "@/components/userInfo/UserDetail";
+export default {
+  components: {
+    Navbar,
+    UserDetail
+  },
+  created() {
+    this.$store.dispatch("fetchUserInfo");
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+}
+</style>
