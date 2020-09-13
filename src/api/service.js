b67d7f61a018acf0fd3c2bed82fa3a5c0d595ea5
yachen <io260188@gmail.com>
2020-09-13 12:23:48 +0800

    feat: replace open source api with kidd api

--- src/api/service.js
+++ src/api/service.js
@@ -1,35 +1,40 @@
 import axios from "axios";
 import router from "../router";
+const domainURL = "https://k88d02.ml/";
 
 const Api = axios.create({
-  baseURL: "http://112.74.99.5:3000/web/api"
+    baseURL: "https://k88d02.ml/api",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
 });
 
+
 Api.interceptors.request.use(
-  function(config) {
-    const id = localStorage.getItem("id");
-    const token = localStorage.getItem("token");
-    if (id && token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    function(config) {
+        const token = localStorage.getItem("remember_token");
+        if (token) {
+            config.headers.Authorization = `${token}`;
+        }
+        return config;
+    },
+    function(error) {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  function(error) {
-    return Promise.reject(error);
-  }
 );
 
 Api.interceptors.response.use(
-  function(response) {
-    return response;
-  },
-  function(error) {
-    // 當 token 失效時，導回登入頁(但有些頁面並不需要 ajax 請求，所以須在 router.beforeEach 另外設定)
-    if (error.response.status === 401 || error.response.status === 402) {
-      router.push("/login");
+    function(response) {
+        return response;
+    },
+    function(error) {
+        // 當 token 失效時，導回登入頁(但有些頁面並不需要 ajax 請求，所以須在 router.beforeEach 另外設定)
+        if (error.response.status === 401 || error.response.status === 402) {
+            router.push("/login");
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
-export default Api;
+export { domainURL, Api };
