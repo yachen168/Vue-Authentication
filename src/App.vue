83e3369922212bf256bde8eafe3caad8df4b3e34
yachen <io260188@gmail.com>
2020-10-16 23:39:21 +0800

    refactor: style of #app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  max-width: 576px;
+  margin: 0 auto;
 }
 </style>
